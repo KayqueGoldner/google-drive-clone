97d4ed8f3bcff8b4ade3e27c8c1186946de4a92a
Kayque Goldner <kayquegoldner@gmail.com>
2024-12-09 07:35:16 -0300

    file details

--- components/action-dropdown.tsx
+++ components/action-dropdown.tsx
@@ -27,6 +27,7 @@ import { constructDownloadUrl } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { renameFile } from "@/lib/actions/file.actions";
+import { FileDetails } from "@/components/actions-modal-content";
 
 export const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -95,6 +96,7 @@ export const ActionDropdown = ({ file }: { file: Models.Document }) => {
               onChange={(e) => setName(e.target.value)}
             />
           )}
+          {value === "details" && <FileDetails file={file} />}
         </DialogHeader>
         {["rename", "delete", "share"].includes(value) && (
           <DialogFooter className="flex flex-col gap-3 md:flex-row">
