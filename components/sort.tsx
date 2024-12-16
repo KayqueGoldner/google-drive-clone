b983ee64fc4ac7c9a29e182b44b4cc521e054be7
Kayque Goldner <kayquegoldner@gmail.com>
2024-12-15 21:41:10 -0300

    sorting

--- components/sort.tsx
+++ components/sort.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { sortTypes } from "@/constants";
+
 export const Sort = () => {
-  return <div>Sort</div>;
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleSort = (value: string) => {
+    router.push(`${pathname}?sort=${value}`);
+  };
+
+  return (
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((sort) => (
+          <SelectItem
+            key={sort.label}
+            className="shad-select-item"
+            value={sort.value}
+          >
+            {sort.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 };
